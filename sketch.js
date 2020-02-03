5b9c7ffd3906009fbd8ca22cff415f0f40b3775b
Yuv360 <60431123+Yuv360@users.noreply.github.com>
2020-02-03 21:52:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var fixedRect,movingRect;
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+ fixedRect =  createSprite(400, 200, 100, 50);
+fixedRect.shapeColor = "green";
+fixedRect.debug = true;
+
+
+movingRect = createSprite(400,150,50,100);
+movingRect.shapeColor = "green";
+movingRect.debug = true;
+
+}
+
+function draw() {
+  background(0,0,0);  
+ 
+movingRect.x = World.mouseX;
+movingRect.y = World.mouseY;
+
+ 
+ 
+ if(istouching(movingRect,fixedRect)){
+movingRect.shapeColor = "red";
+fixedRect.shapeColor = "red";
+ }
+else{
+movingRect.shapeColor = "green";
+fixedRect.shapeColor = "green";
+
+
+
+}
+
+
+
+ 
+  drawSprites();
+
+
+}
+  
